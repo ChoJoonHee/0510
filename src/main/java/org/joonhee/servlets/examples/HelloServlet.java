b263c2c9a575d7e86a9dbfc4b232ae8affecbb8e
joonhee <slaxer@hanmail.net>
2019-04-19 11:11:17 +0900

    회원가입

--- src/main/java/org/joonhee/servlets/examples/HelloServlet.java
+++ src/main/java/org/joonhee/servlets/examples/HelloServlet.java
@@ -2,10 +2,11 @@ package org.joonhee.servlets.examples;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet{
 	
 	public void doGet(HttpServletRequest request,HttpServletResponse response)
